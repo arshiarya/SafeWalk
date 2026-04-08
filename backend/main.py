e04e5f664f04b0ad801ad325c41325c41a20f249
Anushka-dev707 <goyala1001@gmail.com>
2026-04-08 09:20:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,8 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from services.route_service import get_routes
-
-# app = FastAPI()
-
-# # allow frontend
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get("/")
-# def home():
-#     return {"message": "SafeWalk Backend Running 🚀"}
-
-# # MAIN API
-# @app.get("/route")
-# def route(start_lat: float, start_lon: float, end_lat: float, end_lon: float):
-    
-#     result = get_routes(start_lat, start_lon, end_lat, end_lon)
-
-#     return result
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from services.route_service import get_routes, load_graph
+from services.route_service import get_routes
 
 app = FastAPI()
-
-# ✅ Load graph only once
-G = load_graph()
 
 # allow frontend
 app.add_middleware(
@@ -47,7 +17,37 @@ app.add_middleware(
 def home():
     return {"message": "SafeWalk Backend Running 🚀"}
 
+# MAIN API
 @app.get("/route")
 def route(start_lat: float, start_lon: float, end_lat: float, end_lon: float):
-    result = get_routes(start_lat, start_lon, end_lat, end_lon, G)
+    
+    result = get_routes(start_lat, start_lon, end_lat, end_lon)
+
     return result
+
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from services.route_service import get_routes, load_graph
+
+# app = FastAPI()
+
+# # ✅ Load graph only once
+# G = load_graph()
+
+# # allow frontend
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# @app.get("/")
+# def home():
+#     return {"message": "SafeWalk Backend Running 🚀"}
+
+# @app.get("/route")
+# def route(start_lat: float, start_lon: float, end_lat: float, end_lon: float):
+#     result = get_routes(start_lat, start_lon, end_lat, end_lon, G)
+#     return result
