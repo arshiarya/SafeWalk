e34064ae9a2e025ad554a9ffdeda71ac1c24d1ee
Anushka-dev707 <goyala1001@gmail.com>
2026-04-08 09:17:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,38 @@
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from services.route_service import get_routes
+
+# app = FastAPI()
+
+# # allow frontend
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# @app.get("/")
+# def home():
+#     return {"message": "SafeWalk Backend Running 🚀"}
+
+# # MAIN API
+# @app.get("/route")
+# def route(start_lat: float, start_lon: float, end_lat: float, end_lon: float):
+    
+#     result = get_routes(start_lat, start_lon, end_lat, end_lon)
+
+#     return result
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from services.route_service import get_routes
+from services.route_service import get_routes, load_graph
 
 app = FastAPI()
+
+# ✅ Load graph only once
+G = load_graph()
 
 # allow frontend
 app.add_middleware(
@@ -17,10 +47,7 @@ app.add_middleware(
 def home():
     return {"message": "SafeWalk Backend Running 🚀"}
 
-# MAIN API
 @app.get("/route")
 def route(start_lat: float, start_lon: float, end_lat: float, end_lon: float):
-    
-    result = get_routes(start_lat, start_lon, end_lat, end_lon)
-
+    result = get_routes(start_lat, start_lon, end_lat, end_lon, G)
     return result
